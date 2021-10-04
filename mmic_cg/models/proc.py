637d71c94929e1a04fb5aa535288fc62db1dfea4
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-10-04 14:29:17 -0400

    update code format

--- mmic_cg/models/proc.py
+++ mmic_cg/models/proc.py
@@ -3,34 +3,30 @@
 from cmselemental.models import InputProc, OutputProc
 from mmelemental.models import Molecule
 from pydantic import Field
-from typing import Union, Optional, Dict, Any
+from typing import Union, Optional, Dict, List, Any
 
 __all__ = ["InputCoarse", "OutputCoarse"]
 
 
 class InputCoarse(InputProc):
-	"""An input model for coarse-graining."""
+    """An input model for coarse-graining."""
 
-	molecule: Union[Molecule, List[Molecule]] = Field(
-		...,
-		description="A single or list of molecule objects to coarse-grain."
-	)
-	method: str = Field(
-		...,
-		description="The name of the coarse-graining method."
-	)
-	method_keywords: Optional[Dict[str, Any]] = Field(
-		None,
-		description="Additional keyword arguments used for the cg method."
-	)
+    molecule: Union[Molecule, List[Molecule]] = Field(
+        ..., description="A single or list of molecule objects to coarse-grain."
+    )
+    method: str = Field(..., description="The name of the coarse-graining method.")
+    method_keywords: Optional[Dict[str, Any]] = Field(
+        None, description="Additional keyword arguments used for the cg method."
+    )
+
 
 class OutputCoarse(OutputProc):
-	"""An output model for coarse-graining."""
+    """An output model for coarse-graining."""
 
-	proc_input: InputCoarse = Field(
-		...,
-		description=InputCoarse.__doc__,
-	)
-	molecule: Union[Molecule, List[Molecule]] = Field(
-		None, description="A single or list of coarse-grained molecule objects."
-	)
+    proc_input: InputCoarse = Field(
+        ...,
+        description=InputCoarse.__doc__,
+    )
+    molecule: Union[Molecule, List[Molecule]] = Field(
+        None, description="A single or list of coarse-grained molecule objects."
+    )
